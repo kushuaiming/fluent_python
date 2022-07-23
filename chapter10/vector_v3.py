a3191c0df4e0c84c32de34333d12a9dc5715d1df
XuMing <ming.xu@kushuai.com>
2022-07-23 18:05:11 +0800

    feat: chapter10

--- chapter10/vector_v3.py
+++ chapter10/vector_v3.py
@@ -47,14 +47,34 @@ class Vector:
             msg = '{cls.__name__} indices must be integers'
             raise TypeError(msg.format(cls=cls))
 
+    shortcut_names = 'xyzt'
+
+    def __getattr__(self, name):
+        cls = type(self)
+        if len(name) == 1:
+            pos = cls.shortcut_names.find(name)
+            if 0 <= pos < len(self._components):
+                return self._components[pos]
+        msg = '{.__name__!r} objects has no attribute {!r}'
+        raise AttributeError(msg.format(cls, name))
+
+    def __setattr__(self, name, value):
+        cls = type(self)
+        if len(name) == 1:
+            if name in cls.shortcut_names:
+                error = 'readonly attribute {attr_name!r}'
+            elif name.islower():
+                error = "can't set attributes 'a' to 'z' in {cls_name!r}"
+            else:
+                error = ''
+            if error:
+                msg = error.format(cls_name=cls.__name__, attr_name=name)
+                raise AttributeError(msg)
+        super.__setattr__(name, value)
+
+
 @classmethod
 def frombytes(cls, octets):
     typecode = chr(octets[0])
     memv = memoryview(octets[1:]).cast(typecode)
     return cls(memv)
-
-v7 = Vector(range(7))
-print(v7[-1])
-print(v7[1:4])
-print(v7[-1:])
-print(v7[1,2])
