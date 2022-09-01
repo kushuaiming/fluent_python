738a782fc21813dd01d037aea8c885dfd744b863
Ming Xu <ming.xu@allride.ai>
2022-09-01 20:21:18 +0800

    fix: modify string for example2_3.py

--- chapter02/example2_3.py
+++ chapter02/example2_3.py
@@ -1,4 +1,5 @@
-symbols = '!@#$%^&*()'
-beyond_ascii = [ord(symbol) for symbol in symbols if ord(symbol) > 48]
-
+symbols = '$¢£¥€¤'
+beyond_ascii = [ord(symbol) for symbol in symbols if ord(symbol) > 127]
+print(repr(beyond_ascii))
+beyond_ascii = list(filter(lambda c: c > 127, map(ord, symbols)))
 print(repr(beyond_ascii))
